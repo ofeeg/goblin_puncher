3c85e60bb1b295a826fea245a01248939ef502b4
mason ray hanna <mrhan@geefo>
2019-10-27 17:59:47 -0700

    Cleaned up and created makefile for tests

--- tests/ability_test.cc
+++ tests/ability_test.cc
@@ -6,5 +6,6 @@ int main(){
     Ability();
     std::string s = "d";
     Ability(s, TYPES::INT, 1000000000);
+    std::cout << "This message means it constructed an abillity.";
     return 0;
     }
