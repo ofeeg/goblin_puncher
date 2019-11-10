04a414b59fd59ecacbc8cd956ccb28885835042d
ofeeg <mhanna0000@gmail.com>
2019-11-10 09:38:02 -0800

    Added battle functionality

--- tests/battle_test.cc
+++ tests/battle_test.cc
@@ -1,12 +1,28 @@
 #include "../world/battle/battle.hh"
-#include <iostream>
+
 
 std::array<Combatant, 4> Enemies{Combatant(), Combatant(), Combatant()};
 std::array<Combatant, 4> Player{Combatant()};
-
+void InitBattle(BattleScene& bs){
+  bool battling = 1;
+  
+  while(battling){
+    char i;
+    std::cin >> i;
+    switch(i){
+    case('1'):
+      std::cout << "Attack! " << bs.DamageCalculation(Player[0].GetSTR(), Enemies[0].GetDEF());
+      break;
+    case('0'):
+      battling = 0;
+      break;
+    }
+  }
+};
 
 
 int main(){
-  BattleScene(Enemies, Player);
+  BattleScene h(Enemies, Player);
+  InitBattle(h);
   return 0;
 }
--- world/battle/battle.hh
+++ world/battle/battle.hh
@@ -1,9 +1,24 @@
+#ifndef BATTLE
+#define BATTLE
 #include "../character.hh"
+#include <iostream>
+    
+
 
 class BattleScene{
   std::array<Combatant, 4> Enemies;
   std::array<Combatant, 4> Player;
+
 public:
   BattleScene(std::array<Combatant, 4>& e_list, std::array<Combatant, 4> p_list) : Enemies(e_list), Player(p_list){}
+  constexpr int DamageCalculation(int offense, int defense, int abl_dmg=0){
+    int dmg = ((offense*2)) - (defense*3)/2 + abl_dmg;
+    return dmg;
+  }
 };
 
+
+
+
+
+#endif
