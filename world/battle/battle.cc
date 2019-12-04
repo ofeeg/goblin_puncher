12f54efec6c61246a712d3d62176601fee6bb28d
ofeeg <mhanna0000@gmail.com>
2019-12-04 11:31:33 -0800

    pushing changes to work at home computer

--- world/battle/battle.cc
+++ world/battle/battle.cc
@@ -1,15 +1,25 @@
 #include "battle.hh"
 
-
-void Battle::InitPassive(Combatant& a){
-  if(!(a.GetPassiveAbilities.compare("")){
-    for(Ability*  i : a.Abilities.first){
+void BattleScene::InitPassive(Combatant& a){
+  if(!(a.GetPassiveAbilities.compare(""))){
+      for(Ability*  i : a.Abilities.first)){
       if(i->ScaleType = TYPES::PAS){
         std::cout << "Doing something unique \n";
        }
        else{
 	  std::cout << i->ScaleDamage;
-	}
-     }
-   }
+       }
+  }
 }
+
+
+void BattleScene::SetTurnOrder(){
+  std::vector<int> Order;
+  for(Combatant& i : this->Player){
+    Order.push_back(i.GetSPD());
+  }
+  for(Combatant& i : this->Enemies){
+    Order.push_back(i.GetSPD());
+  }
+  for(auto& i : Order)
+  
--- world/battle/battle.hh
+++ world/battle/battle.hh
@@ -5,16 +5,59 @@
 #include <stdexcept>    
 
 
+struct TurnQueue{
+  const Combatant EMPTY = Combatant();
+  std::vector<Combatant*> Order{EMPTY};
+  inline void enqueue(Combatant& c){
+    bool filled = 1;
+    unsigned short index = 0;
+    while(filled){
+      if(Order[index].GetName().compare(EMPTY.GetName())){
+	Order[index] = &c;
+	Order.push_back(&EMPTY);
+	filled = 0;
+      }
+      index++
+	}
+  }
+  inline Combatant dequeue(){
+    if(Order.front()->GetName().compare(EMPTY.GetName())){
+      throw std::runtime_error("Empty queue, cannot dequeue");
+    }
+    Combatant* tmp;
+    Combatant return_val = *Order.front();
+    bool IsCurrentTurnHandled = 0;
+    short index  = 0;
+    for(Combatant* it; it != Order.at(Order.size()-2)){
+      if(!IsCurrentTurnHandled){
+	tmp = *Order.at(Order.size()-2);
+	Order.at(Order.size()-2) = it;
+	IsCurrentTurnHandled = 1;
+      }
+      else{
+	Order.at(index) = it;
+	index++;
+      }
+    }
+    Order.at(Order.size()-3) = *tmp;
+    return return_value;
+  }
+};
+    
+    
+
 class BattleScene{
   std::array<Combatant, 4> Enemies;
   std::array<Combatant, 4> Player;
 public:
   BattleScene(std::array<Combatant, 4>& e_list, std::array<Combatant, 4> p_list) : Enemies(e_list), Player(p_list){}
+  TurnQueue Turns;
   constexpr int DamageCalculation(int offense, int defense, int abl_dmg=0){
     int dmg = ((offense*2)) - (defense*3)/2 + abl_dmg;
     return dmg;
   }
   void InitPassive(Combatant& a);
+  void SetTurnOrder();
 };
 
 
